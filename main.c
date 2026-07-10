f74a1ad6ca6df7a523b5c9d456fdbabe7bb7e232
Voltz1037 <3077195024@qq.com>
2026-07-10 16:14:13 +0800

    update output message

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 //#include <readdir.h>
 
 int key;
-int x = 0, y = 0; // 初始化光标位置
+int x = 2, y = 2; // 初始化光标位置
 WINDOW* window;
 void printpath(WINDOW* window);
 void printtime(WINDOW* window);
@@ -21,18 +21,31 @@ void* uscanf(void*){
                 switch (key){
                         case KEY_UP:
                                 wmove(window, y--, x);
+                                if(y==1){
+                                    y++;
+                                }
                                 break;
                         case KEY_DOWN:
                                 wmove(window, y++, x);
                                 break;
                         case KEY_LEFT:
                                 wmove(window, y, x--);
+                                if(x==1){
+                                    x=67;y--;
+                                }
                                 break;
                         case KEY_RIGHT:
                                 wmove(window, y, x++);
+                                if(x==69){
+                                    x=1;y++;
+                                }
                                 break;
                         default:
                                 waddch(window, key);
+                                x++;
+                                if(x==68){
+                                    x=1;y++;
+                                }
                                 break;
                 }
                 refresh();
@@ -58,7 +71,7 @@ int main()
         noecho();            // 禁用回显  
         keypad(stdscr,1); // 启用功能键（如方向键）
         start_color(); // 启用颜色功能
-        init_pair(1, COLOR_GREEN, COLOR_WHITE);
+        
         init_pair(2, COLOR_BLACK, COLOR_WHITE);
         // 创建一个新窗口
     window = newwin(50, 70, 0, 0);
@@ -95,6 +108,7 @@ void printtime(WINDOW* window)
 
 void printpath(WINDOW* window){
 char path[105];
+init_pair(1, COLOR_GREEN, COLOR_WHITE);
 getcwd(path,sizeof(path));
 if((path[0]=='/')&&(path[1]=='h')&&(path[2]=='o')&&(path[3]=='m')&&(path[4]=='e')){
     for(int i=0;i<101;i++){
@@ -102,7 +116,7 @@ if((path[0]=='/')&&(path[1]=='h')&&(path[2]=='o')&&(path[3]=='m')&&(path[4]=='e'
     }
      path[0]='~';
 }
-//attron(COLOR_PAIR(1) | A_BOLD);
+attron(COLOR_PAIR(1));
 mvwprintw(window, 11, 1, "%s", path);
-//attroff(COLOR_PAIR(1) | A_BOLD);  // 关闭文本属性
+attroff(COLOR_PAIR(1));  // 关闭文本属性
 }

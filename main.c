8ac9675d7c40d6634d19640807f651b8fe53f747
Voltz1037 <3077195024@qq.com>
2026-07-10 15:33:54 +0800

    update output message

--- main.c
+++ main.c
@@ -4,33 +4,17 @@
 #include <sys/types.h>   
 #include <dirent.h>
 #include <time.h>
+#include <pthread.h>
+#include<unistd.h>
 //#include <readdir.h>
 
+int key;
+int x = 0, y = 0; // 初始化光标位置
+WINDOW* window;
 void printpath(WINDOW* window);
 void printtime(WINDOW* window);
-void uscanf(void);
-void refresh_time(void);
-int main()
-{
-        int key;
-         int x = 0, y = 0; // 初始化光标位置
-        //初始化ncurses
-        setlocale(LC_ALL,"");
-        initscr();
-        cbreak();            // 禁用行缓冲  
-        noecho();            // 禁用回显  
-        keypad(stdscr,1); // 启用功能键（如方向键）
-        start_color(); // 启用颜色功能
-        init_pair(1, COLOR_GREEN, COLOR_WHITE);
-        init_pair(2, COLOR_BLACK, COLOR_WHITE);
-        // 创建一个新窗口
-    WINDOW* window = newwin(50, 70, 0, 0);
-     box(window, 0, 0); // 绘制窗口边框
-      // 在窗口中显示文本
-    mvwprintw(window, 1, 1, "对话区：");
-    mvwprintw(window, 10, 1, "--------------------------------------------------------------------");
-    
-        //循环等待用户输入
+void* uscanf(void*){
+ //循环等待用户输入
         while(key != 'q') {
         
                 key = getch();
@@ -51,13 +35,45 @@ int main()
                                 waddch(window, key);
                                 break;
                 }
-                //刷新屏幕以显示新输出
-                printtime(window);
-                wmove(window, y, x);
                 refresh();
                 wrefresh(window);
         }
- 
+}
+void* refresh_time(void*){
+while (1){
+delay_output(1000);
+printtime(window);
+wmove(window, y, x);
+wrefresh(window);
+}
+
+}
+int main()
+{
+        
+        //初始化ncurses
+        setlocale(LC_ALL,"");
+        initscr();
+        cbreak();            // 禁用行缓冲  
+        noecho();            // 禁用回显  
+        keypad(stdscr,1); // 启用功能键（如方向键）
+        start_color(); // 启用颜色功能
+        init_pair(1, COLOR_GREEN, COLOR_WHITE);
+        init_pair(2, COLOR_BLACK, COLOR_WHITE);
+        // 创建一个新窗口
+    window = newwin(50, 70, 0, 0);
+     box(window, 0, 0); // 绘制窗口边框
+      // 在窗口中显示文本
+    mvwprintw(window, 1, 1, "对话区：");
+    mvwprintw(window, 10, 1, "--------------------------------------------------------------------");
+    printpath(window);
+
+
+
+    pthread_t t1,t2;
+    pthread_create(&t1,NULL,refresh_time,NULL);
+    pthread_create(&t2,NULL,uscanf,NULL);
+    pthread_join(t2,NULL);
         //结束ncurses模式
         endwin();
         return 0;
@@ -69,24 +85,16 @@ void printtime(WINDOW* window)
     time_t now = time(NULL);
     struct tm *local_time = localtime(&now);
     char time_str[20];
+    char* timedisplay;
+    timedisplay=&time_str[11];
     strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", local_time);
     // 在窗口中显示时间
-    mvwprintw(window, 2, 1, "当前时间：%s", time_str);
+    mvwprintw(window, 11, 61, "%s",timedisplay);
+    wmove(window, y, x);
 }
-/*void printpath(WINDOW* window)
-{
-    // 获取目录中所有的文件路径
-    const char *dir_path = "111";
-    pathInfo *path_info = init_head_node();
-    read_dir_ex(dir_path, path_info);
-    //printf("==========================\n");
-    pathInfo *info = path_info->next;
-    //printf("head: %p, %p\n", path_info->pre, path_info->next);
-    mvwprintw(window, 3, 1, "head: %p, %p\n", path_info->pre, path_info->next);
-    while(info) {
-        mvwprintw(window, 4, 1, "%p, %p, %p, %d, %s\n", info, info->pre, info->next, info->isdir, info->name);
-        info = info->next;
-    }
-    free_memory(path_info); // 释放内存空间
 
-}*/
+void printpath(WINDOW* window){
+char path[105];
+getcwd(path,sizeof(path));
+mvwprintw(window, 11, 1, "%s", path);
+}
